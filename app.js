99f68f8c0716bfed588ebdd4cf6a2a704d95b4b2
ChrisThatGamer1 <50189496+ChrisThatGamer1@users.noreply.github.com>
2022-08-16 19:51:54 -0400

    First Commit (test)
    Testing to make sure the template admin page that I created works when running the program. Now looking over user case to see first steps in solving use case 2 - "Alt Flows."

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var adminRouter = require('./routes/admin');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
